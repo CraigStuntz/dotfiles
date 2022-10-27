041662b57f80c56e1b2c52ae80fff7fc2fab42d4
Craig Stuntz <CraigStuntz@users.noreply.github.com>
2022-10-26 22:55:55 -0400

    Eval after installing homebrew

--- provision/provision.sh
+++ provision/provision.sh
@@ -5,6 +5,8 @@ if ! command -v brew &> /dev/null
 then
     echo "[provision.sh] Installing homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 if ! command -v ansible &> /dev/null
